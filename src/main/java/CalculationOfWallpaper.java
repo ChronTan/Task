8122135cf0663091c406117aeae1fcb8d9da4fd8
NoName <skarr.age@mail.ru>
2019-11-10 19:56:43 +0300

    fix bugs

--- src/main/java/CalculationOfWallpaper.java
+++ src/main/java/CalculationOfWallpaper.java
@@ -1,16 +1,15 @@
-import java.util.Scanner;
 
 public class CalculationOfWallpaper {
 
-    public static void main(String[] args) {
-        CalculationOfWallpaper calculationOfWallpaper=new CalculationOfWallpaper();
-        double heightRoom=5.0;
-        double widthRoom=7.0;
-        double lengthRoom=6.0;
-        double widthWallpaper=1.0;
-        double lengthWallpaper=10.0;
-
-    }
+//    public static void main(String[] args) {
+//        CalculationOfWallpaper calculationOfWallpaper=new CalculationOfWallpaper();
+//        double heightRoom=5.0;
+//        double widthRoom=7.0;
+//        double lengthRoom=6.0;
+//        double widthWallpaper=1.0;
+//        double lengthWallpaper=10.0;
+//
+//    }
 
     public double countR(double lengthRoom, double widthRoom,double widthWallpaper, double lengthWallpaper, double heightRoom){
         double totalRolls=1.0;
